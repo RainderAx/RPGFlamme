d296d93f0a01991a0fa12af86fd06f9257c3edce
Rainder <sbst1.elz@gmail.com>
2026-07-19 18:50:46 +0200

    UwU

--- src/battleSystem/Main.java
+++ src/battleSystem/Main.java
@@ -9,21 +9,22 @@ import Entity.*;
 
 public class Main {
     public static void main(String[] args) {
+
         List<Entity> heroes = new ArrayList<>();
-        heroes.add(new Flamme("Flamme le Prince d'Hoturan", 400, 30, 10));
-        heroes.add(new Tching("Tching", 350, 30, 5));
-        heroes.add(new Bob("Bob le chef", 600, 20, 20));
+        heroes.add(new Flamme("Flamme le Prince d'Hoturan", 400, 10, 10));
+        heroes.add(new Tching("Tching", 350, 10, 5));
+        heroes.add(new Bob("Bob le chef", 600, 10, 20));
 
         List<Entity> monsters = new ArrayList<>();
-        monsters.add(new Gobelin("GobA", 150, 20, 10));
-        
-        monsters.add(new chefGobelin("GobC", 300, 40, 20));
+        monsters.add(new Gobelin("Gobelin", 150, 20, 10));
+        monsters.add(new chefGobelin("Chef Gobelin", 300, 40, 20));
 
         JFrame frame = new JFrame("RPG Battle System - Team Fight");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 800);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
+
 
         JeuImage gamePanel = new JeuImage(heroes, monsters);
         frame.add(gamePanel);
